dec9de0b74d0bcfb6226e4c490de80098929ee93
Badri Vishal Singhal <87443929+vishal8803@users.noreply.github.com>
2022-05-25 12:29:22 +0530

    Time: 472 ms (56.49%), Space: 77.6 MB (70.83%) - LeetHub

--- 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
@@ -7,31 +7,8 @@ public:
     int maxEnvelopes(vector<vector<int>>& env) {
         int n = env.size();
         
-        // sorting by height & if we encounter same height
-        // sort by descending order of width
+        
         sort(env.begin(), env.end(), cmp);
-        
-        // for(vector<int> e : env)
-        // {
-        //     cout<<e[0]<<" "<<e[1]<<endl;
-        // }
-        
-        // e.g. -> env => (3,8) (4,5) (2,1) (2,6) (7,8) (5,3) (5,7)
-        // sorted version => (2,6) (2,1) (3,8) (4,5) (5,7) (5,3) (7,8)
-        
-        // Now, we only need to care about width
-        // So, as of now we only need to look upon v[i][1]
-        // [ 6, 1, 8, 5, 7, 3, 8 ]
-        
-        // Now, we can only put envolop a in envolop b another if width of a is
-        // less than width of b, or we can say we need an envolop whose width
-        // is just greater than envolop a ( height we have already handled )
-        // So, we can think of lower_bound and Longest Increasing Subsequence
-        
-        // because we have sorted all envolopes of a particular height
-        // by descending order of width, one envolope will not interfare with 
-        // another envolop of same height, if we apply lower_bound
-        // i.e. first elem greater than equal to itself in lis
         
         vector<int> lis;
         
