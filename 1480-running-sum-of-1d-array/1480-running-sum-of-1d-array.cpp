c2dbf8ccd985bfb81ae5b39763bd600e7cf43bcc
Badri Vishal Singhal <87443929+vishal8803@users.noreply.github.com>
2022-06-01 11:28:00 +0530

    Time: 6 ms (32.99%), Space: 8.6 MB (38.06%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        vector<int> ans;
+        ans.push_back(nums[0]);
+        
+        for(int i = 1; i < nums.size(); i++){
+            ans.push_back(nums[i] + ans[ans.size()-1]);
+        }
+        
+        return ans;
+    }
+};
