fc37cbd621f5491ef21320b63b7875de26a7e7f8
Badri Vishal Singhal <87443929+vishal8803@users.noreply.github.com>
2022-07-08 20:52:47 +0530

    Time: 3 ms (62.29%), Space: 6.7 MB (67.51%) - LeetHub

---
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int countWays(vector<int> &nums, int target, vector<int> &dp){
+        if(target==0)
+            return 1;
+        
+        if(target<0)
+            return 0;
+        
+        if(dp[target] != -1)
+            return dp[target];
+        
+        int cnt = 0;
+        for(int i = 0; i<nums.size(); i++){
+            cnt += countWays(nums,target-nums[i],dp);
+        }
+        return dp[target] = cnt;
+    }
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int> dp(target+1,-1);
+        return countWays(nums,target,dp);
+    }
+};
