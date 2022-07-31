4fe7cc96cfe5120c099dc8362b97ce512413b0f0
Badri Vishal Singhal <87443929+vishal8803@users.noreply.github.com>
2022-07-31 16:52:11 +0530

    Time: 163 ms (45.91%), Space: 5.9 MB (81.16%) - LeetHub

--- 526-beautiful-arrangement/526-beautiful-arrangement.cpp
+++ 526-beautiful-arrangement/526-beautiful-arrangement.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    void calc(int n, int &count, vector<bool> &visited, int idx){
-        if(idx==n){
+    void calc(int n, vector<bool> &visited, int &count, int idx) {
+        if(idx==n) {
             count++;
             return;
         }
-        
-        for(int i = 1; i <= n; i++){
-            if(!visited[i]){
+        for(int i = 1; i <= n; i++) {
+            if(!visited[i]) {
                 if(i%(idx+1)==0 || (idx+1)%i==0){
                     visited[i] = true;
-                    calc(n,count,visited,idx+1);
+                    calc(n,visited,count,idx+1);
                     visited[i] = false;
                 }
             }
         }
     }
+    
     int countArrangement(int n) {
-        vector<bool> visited(n+1);
         int count = 0;
-        calc(n,count,visited,0);
+        vector<bool> visited(n+1,false);
+        calc(n,visited,count,0);
         return count;
     }
 };
