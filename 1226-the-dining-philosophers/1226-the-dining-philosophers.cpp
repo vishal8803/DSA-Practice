72ca08bf93a01b8f007e6f082847bcb23eeea57d
Badri Vishal Singhal <87443929+vishal8803@users.noreply.github.com>
2022-08-13 16:55:15 +0530

    Time: 357 ms (26.72%), Space: 10.6 MB (97.20%) - LeetHub

--- 1226-the-dining-philosophers/1226-the-dining-philosophers.cpp
+++ 1226-the-dining-philosophers/1226-the-dining-philosophers.cpp
@@ -1,37 +1,41 @@
 class Semaphore {
-    std::mutex m;
-    std::condition_variable cv;
-    int count;
+    private:
+        int count;
+        condition_variable cv;
+        mutex m;
     
     public:
-    Semaphore(){}
-
-    void setCount(int a){
-        count = a;
-    }
-
-    void signal(){
-        count++;
-        if(count<=0){
-            cv.notify_one();
+        
+        Semaphore() {}
+        
+        void setCount(int a) {
+            count = a;
         }
-    }
-
-    void wait(){
-        std:unique_lock<std::mutex> lock(m);
-        count--;
-        while(count<0){
-            cv.wait(lock);
+        
+        inline void wait() {
+            unique_lock<mutex> lock(m);
+            count--;
+            while(count < 0)
+                cv.wait(lock);
         }
-    }
+    
+        inline void signal() {
+            unique_lock<mutex> lock(m);
+            count++;
+            if(count<=0)
+                cv.notify_one();
+        }
+    
 };
 
 class DiningPhilosophers {
-    Semaphore fork[5];
-    std::mutex m;
 public:
+    Semaphore fork[5];
+    mutex m;
+    condition_variable cv;
+    
     DiningPhilosophers() {
-        for(int i = 0; i < 5; i++){
+        for(int i = 0; i < 5; i++) {
             fork[i].setCount(1);
         }
     }
@@ -42,18 +46,19 @@ public:
                     function<void()> eat,
                     function<void()> putLeftFork,
                     function<void()> putRightFork) {
+		
         {
-            std::lock_guard<std::mutex>lock(m);
+            lock_guard<mutex> lock(m);
             fork[philosopher].wait();
-            fork[(philosopher + 1) % 5].wait();
+            fork[(philosopher+1)%5].wait();
         }
         
         pickLeftFork();
         pickRightFork();
-        
         eat();
         putLeftFork();
-        fork[(philosopher + 1) % 5].signal();
+        fork[(philosopher+1)%5].signal();
+        
         putRightFork();
         fork[philosopher].signal();
     }
