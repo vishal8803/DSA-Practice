9347a60646be700714591fac1064df23885cb085
Badri Vishal Singhal <87443929+vishal8803@users.noreply.github.com>
2022-08-13 12:45:00 +0530

    Time: 368 ms (15.87%), Space: 7.3 MB (19.05%) - LeetHub

--- 1114-print-in-order/1114-print-in-order.cpp
+++ 1114-print-in-order/1114-print-in-order.cpp
@@ -1,37 +1,47 @@
-class Foo {
-    std::mutex m;
-    std::condition_variable cv;
-    int turn;
-public:
-    Foo() {
+class Foo
+{
+    public:
+        std::mutex m;
+        std::condition_variable cv;
+        int turn;
+
+    Foo()
+    {
         turn = 0;
     }
 
-    void first(function<void()> printFirst) {
-        
-        // printFirst() outputs "first". Do not change or remove this line.
+    void first(function < void() > printFirst)
+    {
+
+       	// printFirst() outputs "first". Do not change or remove this line.
         printFirst();
         turn = 1;
         cv.notify_all();
     }
 
-    void second(function<void()> printSecond) {
+    void second(function < void() > printSecond)
+    {
+
+       	// printSecond() outputs "second". Do not change or remove this line.
         std::unique_lock<std::mutex> lock(m);
-        while(turn != 1){
+        while (turn != 1)
+        {
             cv.wait(lock);
         }
-        // printSecond() outputs "second". Do not change or remove this line.
         printSecond();
         turn = 2;
         cv.notify_all();
     }
 
-    void third(function<void()> printThird) {
+    void third(function < void() > printThird)
+    {
+
+       	// printThird() outputs "third". Do not change or remove this line.
         std::unique_lock<std::mutex> lock(m);
-        while(turn != 2){
+        while (turn != 2)
+        {
             cv.wait(lock);
         }
-        // printThird() outputs "third". Do not change or remove this line.
         printThird();
     }
 };
