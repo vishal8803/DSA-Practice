f7f42991e2df77cedf419f74bff80948d3c9f390
Badri Vishal Singhal <87443929+vishal8803@users.noreply.github.com>
2022-08-15 09:27:56 +0530

    Time: 8 ms (84.03%), Space: 6.1 MB (64.52%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,35 +1,54 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        vector<pair<char,int>> v = {{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
         int ans = 0;
-        int n = s.length();
+        int i = 0;
         
-        for(int i = 0; i < s.length(); i++) {
-            if(i+1<n && s[i]=='I' && s[i+1]=='V'){
-                ans += 4;
-                i+=1;
-            }else if(i+1<n && s[i]=='I' && s[i+1]=='X'){
-                ans += 9;
-                i+=1;
-            }else if(i+1<n && s[i]=='X' && s[i+1]=='L'){
-                ans += 40;
-                i+=1;
-            }else if(i+1<n && s[i]=='X' && s[i+1]=='C'){
-                ans += 90;
-                i+=1;
-            }else if(i+1<n && s[i]=='C' && s[i+1]=='D'){
-                ans += 400;
-                i+=1;
-            }else if(i+1<n && s[i]=='C' && s[i+1]=='M'){
-                ans += 900;
-                i+=1;
-            }else{
-                for(auto j : v){
-                    if(s[i]==j.first){
-                        ans += j.second;
-                        break;
-                    }
+        while(i < s.length()) {
+            if(s[i]=='V') {
+                ans += 5;
+                i++;
+            } else if(s[i]=='L') {
+                ans += 50;
+                i++;
+            } else if (s[i]=='D') {
+                ans += 500;
+                i++;
+            } else if(s[i]=='M') {
+                ans += 1000;
+                i++;
+            } else if(s[i]=='I') {
+                if(i+1 < s.size() && s[i+1]=='V') {
+                    ans += 4;
+                    i+=2;
+                } else if(i+1 < s.size() && s[i+1]=='X') {
+                    ans += 9;
+                    i+=2;
+                } else {
+                    ans += 1;
+                    i++;
+                }
+            } else if(s[i]=='X') {
+                if(i+1 < s.size() && s[i+1]=='L') {
+                    ans += 40;
+                    i+=2;
+                } else if(i+1 < s.size() && s[i+1]=='C') {
+                    ans += 90;
+                    i+=2;
+                } else {
+                    ans += 10;
+                    i++;
+                }
+            } else if(s[i]=='C') {
+                if(i+1 < s.size() && s[i+1]=='D') {
+                    ans += 400;
+                    i+=2;
+                } else if(i+1 < s.size() && s[i+1]=='M') {
+                    ans += 900;
+                    i+=2;
+                } else {
+                    ans += 100;
+                    i++;
                 }
             }
         }
