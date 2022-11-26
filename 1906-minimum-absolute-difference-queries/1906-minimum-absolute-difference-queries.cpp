420b3eb5869e0cd31d5120661df49474e6a581d3
Badri Vishal Singhal <87443929+vishal8803@users.noreply.github.com>
2022-11-26 11:32:20 +0530

    Time: 2119 ms (8.15%), Space: 455.4 MB (5.19%) - LeetHub

--- 1906-minimum-absolute-difference-queries/1906-minimum-absolute-difference-queries.cpp
+++ 1906-minimum-absolute-difference-queries/1906-minimum-absolute-difference-queries.cpp
@@ -12,25 +12,21 @@ public:
         }
         
         vector<int> ans;
-        // return ans;
         for(auto q : queries) {
             int lt = q[0];
             int rt = q[1];
             vector<int> freq(mx + 1, 0);
             vector<int> t;
-            // cout<<n<<" "<<rt + 1<<" "<<lt<<endl;
             for(int i = 0; i < mx + 1; i++) {
                 if(lt==0)
                 freq[i] = abs(mp[rt + 1][i]);
                 else freq[i] = abs(mp[rt + 1][i] - mp[lt][i]);
                 if(freq[i] > 0) t.push_back(i);
             }
-            // cout<<"h"<<endl;
             int mn = INT_MAX;
             for(int i = 0; i < t.size() - 1; i++) {
                 mn = min(mn, abs(t[i] - t[i+1]));
             }
-            // cout<<mn<<endl;
             if(mn == INT_MAX)
                 ans.push_back(-1);
             else ans.push_back(mn);
