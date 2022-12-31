82728ac1a6eb053c35e93b25214480751647b8e5
Badri Vishal Singhal <87443929+vishal8803@users.noreply.github.com>
2022-12-31 09:56:10 +0530

    Added solution - LeetHub

---
+++ Shortest path in Undirected Graph having unit distance - GFG/shortest-path-in-undirected-graph-having-unit-distance.cpp
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    vector<int> shortestPath(vector<vector<int>>& edges, int N,int M, int src){
+        vector<int> dist(N, INT_MAX);
+        dist[src] = 0;
+        vector<int> adj[N];
+        
+        for(auto edge : edges) {
+            int x = edge[0];
+            int y = edge[1];
+            // int wt = edge[2];
+            adj[x].push_back(y);
+            adj[y].push_back(x);
+        }
+        
+        queue<int> q;
+        q.push(src);
+        
+        while(q.size() > 0) {
+            int node = q.front();
+            q.pop();
+            
+            for(auto it : adj[node]) {
+                if(dist[it] > dist[node] + 1) {
+                    dist[it] = dist[node] + 1;
+                    q.push(it);
+                }
+            }
+        }
+        
+        for(int i = 0; i < N; i++) {
+            if(dist[i] == INT_MAX) dist[i] = -1;
+        }
+        
+        return dist;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m; cin >> n >> m;
+        vector<vector<int>> edges;
+
+        for (int i = 0; i < m; ++i) {
+            vector<int> temp;
+            for(int j=0; j<2; ++j){
+                int x; cin>>x;
+                temp.push_back(x);
+            }
+            edges.push_back(temp);
+        }
+
+        int src; cin >> src;
+
+        Solution obj;
+
+        vector<int> res = obj.shortestPath(edges, n, m, src);
+
+        for (auto x : res){
+            cout<<x<<" ";
+        }
+        cout << "\n";
+    }
+}
+
+// } Driver Code Ends
