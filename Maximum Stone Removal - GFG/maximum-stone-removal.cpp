e38f7a3452017c1e7d57ab3fb0e2b85e01e71265
Badri Vishal Singhal <87443929+vishal8803@users.noreply.github.com>
2023-01-02 00:14:13 +0530

    Added solution - LeetHub

---
+++ Maximum Stone Removal - GFG/maximum-stone-removal.cpp
@@ -0,0 +1,88 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+  vector<int> parent;
+    vector<int> rnk;
+    void makeset(int n){
+        parent = vector<int>(n);
+        rnk = vector<int>(n);
+        
+        for(int i = 0; i < n; i++)
+            parent[i] = i;
+        
+        for(int i = 0; i <n; i++)
+            rnk[i] = 0;
+    }
+    
+    int find(int n){
+        if(parent[n]==n){
+            return n;
+        }
+        
+        return parent[n] = find(parent[n]);
+    }
+    
+    void makeUnion(int u, int v){
+        u = find(u);
+        v = find(v);
+        
+        if(rnk[u] > rnk[v]){
+            parent[v] = u;
+        }else if(rnk[v] > rnk[u]){
+            parent[u] = v;
+        }else{
+            parent[v] = u;
+            rnk[u]++;
+        }
+    }
+    
+    int removeStones(vector<vector<int>>& stones) {
+        makeset(20005);
+        for(vector<int> stone : stones){
+            makeUnion(stone[0],stone[1]+10001);
+        }
+        set<int> st;
+        for(vector<int> stone : stones){
+            st.insert(find(stone[0]));
+        }
+        
+        return stones.size()-st.size();
+    }
+    int maxRemove(vector<vector<int>>& stones, int n) {
+        return removeStones(stones);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<vector<int>> adj;
+
+        for (int i = 0; i < n; ++i) {
+            vector<int> temp;
+            for (int i = 0; i < 2; ++i) {
+                int x;
+                cin >> x;
+                temp.push_back(x);
+            }
+            adj.push_back(temp);
+        }
+
+        Solution obj;
+
+        cout << obj.maxRemove(adj, n) << "\n";
+    }
+}
+// } Driver Code Ends
