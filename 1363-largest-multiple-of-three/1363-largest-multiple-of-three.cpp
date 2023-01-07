090a907f8f48118eb7c0411b0aa3cc83bdf350f2
Badri Vishal Singhal <87443929+vishal8803@users.noreply.github.com>
2023-01-07 13:50:32 +0530

    Time: 47 ms (35.84%), Space: 19.6 MB (72.25%) - LeetHub

---
+++ 1363-largest-multiple-of-three/1363-largest-multiple-of-three.cpp
@@ -0,0 +1,103 @@
+class Solution {
+public:
+    string calc(vector<int>& digits) {
+        int sum = 0;
+        sort(digits.begin(), digits.end());
+        for(int i : digits) {
+            sum += i;
+        }
+        
+        if(sum%3==0) {
+            string ans = "";
+            for(int i : digits) ans += to_string(i);
+            sort(ans.begin(), ans.end(), greater<char>());
+            return ans;
+        } else if(sum%3==1) {
+            bool f1 = false;
+            for(int i : digits) {
+                if(i%3==1) {
+                    f1 = true;
+                    break;
+                }
+            }
+            if(f1) {
+                int count = 0;
+                string ans = "";
+                for(int i : digits) {
+                    if(i%3==1 and count == 0) {
+                        count++;
+                        continue;
+                    }
+                    ans += to_string(i);
+                }
+                sort(ans.begin(), ans.end(), greater<char>());
+                return ans;
+            }
+            int count = 0;
+            for(int i : digits) {
+                if(i%3==2) count++;
+            }
+            if(count >= 2) {
+                count = 0;
+                string ans = "";
+                for(int i : digits) {
+                    if(i%3==2 and count < 2) {
+                        continue;
+                    }
+                    ans += to_string(i);
+                }
+                sort(ans.begin(), ans.end(), greater<char>());
+                return ans;
+            }
+            return "";
+        } else if(sum%3==2) {
+            bool f1 = false;
+            for(int i : digits) {
+                if(i%3==2) {
+                    f1 = true;
+                    break;
+                }
+            }
+            if(f1) {
+                int count = 0;
+                string ans = "";
+                for(int i : digits) {
+                    if(i%3==2 and count == 0) {
+                        count++;
+                        continue;
+                    }
+                    ans += to_string(i);
+                }
+                sort(ans.begin(), ans.end(), greater<char>());
+                return ans;
+            }
+            int count = 0;
+            for(int i : digits) {
+                if(i%3==1) count++;
+            }
+            if(count >= 2) {
+                count = 0;
+                string ans = "";
+                for(int i : digits) {
+                    if(i%3==1 and count < 2) {
+                        continue;
+                    }
+                    ans += to_string(i);
+                }
+                sort(ans.begin(), ans.end(), greater<char>());
+                return ans;
+            }
+            return "";
+        }
+        return "";
+    }
+    
+    
+    string largestMultipleOfThree(vector<int> &digits) {
+        string ans = calc(digits);
+        int i = 0;
+        while(i < ans.size() - 1 and ans[i] == '0') i++;
+        
+        return ans.substr(i);
+    }
+};
