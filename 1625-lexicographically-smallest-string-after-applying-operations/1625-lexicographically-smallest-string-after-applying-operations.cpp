1d99efb0a6df3c8d9edadccbe585c2d0465fe117
Badri Vishal Singhal <87443929+vishal8803@users.noreply.github.com>
2023-01-21 12:01:59 +0530

    Time: 461 ms (54.37%), Space: 113.7 MB (29.13%) - LeetHub

--- 1625-lexicographically-smallest-string-after-applying-operations/1625-lexicographically-smallest-string-after-applying-operations.cpp
+++ 1625-lexicographically-smallest-string-after-applying-operations/1625-lexicographically-smallest-string-after-applying-operations.cpp
@@ -16,32 +16,29 @@ public:
         st.insert(s);
         
         while(q.size() > 0) {
-            int size = q.size();
-            while(size--) {
-                string temp = q.front();
-                q.pop();
-                
-                string s1 = rotateString(temp, b); // only rotated
-                string s2 = temp; // only added
-                
-                
-                
-                for(int i = 1; i < temp.size(); i += 2) {
-                    s2[i] = '0' + (((s2[i] - '0') + a)%10);
-                }
-                
-                
-                if(st.find(s1) == st.end()) {
-                    if(ans > s1) ans = s1;
-                    st.insert(s1);
-                    q.push(s1);
-                }
-                
-                if(st.find(s2) == st.end()) {
-                    if(ans > s2) ans = s2;
-                    st.insert(s2);
-                    q.push(s2);
-                }
+            string temp = q.front();
+            q.pop();
+
+            string s1 = rotateString(temp, b); // only rotated
+            string s2 = temp; // only added
+
+
+
+            for(int i = 1; i < temp.size(); i += 2) {
+                s2[i] = '0' + (((s2[i] - '0') + a)%10);
+            }
+
+
+            if(st.find(s1) == st.end()) {
+                if(ans > s1) ans = s1;
+                st.insert(s1);
+                q.push(s1);
+            }
+
+            if(st.find(s2) == st.end()) {
+                if(ans > s2) ans = s2;
+                st.insert(s2);
+                q.push(s2);
             }
         }
         
