d92d3373d6f6f9c49ccd26d690a06b8b133bfd74
Badri Vishal Singhal <87443929+vishal8803@users.noreply.github.com>
2023-01-21 11:32:26 +0530

    Time: 452 ms (55.34%), Space: 113.7 MB (29.13%) - LeetHub

---
+++ 1625-lexicographically-smallest-string-after-applying-operations/1625-lexicographically-smallest-string-after-applying-operations.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    string rotateString(string s, int b) {
+        int n = s.size();
+        s = s.substr(n-b) + s.substr(0, n - b);
+        return s;
+    }
+    
+    string findLexSmallestString(string s, int a, int b) {
+        queue<string> q;
+        q.push(s);
+        
+        string ans = s;
+        
+        set<string> st;
+        st.insert(s);
+        
+        while(q.size() > 0) {
+            int size = q.size();
+            while(size--) {
+                string temp = q.front();
+                q.pop();
+                // cout<<temp<<endl;
+                string s1 = rotateString(temp, b); // only rotated
+                string s2 = temp; // only added
+                
+                
+                
+                for(int i = 1; i < temp.size(); i += 2) {
+                    s2[i] = '0' + (((s2[i] - '0') + a)%10);
+                }
+                
+                // string s3 = rotateString(s2, b); //  added and rotated
+                
+                if(st.find(s1) == st.end()) {
+                    if(ans > s1) ans = s1;
+                    st.insert(s1);
+                    q.push(s1);
+                }
+                if(st.find(s2) == st.end()) {
+                    if(ans > s2) ans = s2;
+                    st.insert(s2);
+                    q.push(s2);
+                }
+                // if(st.find(s3) == st.end()) {
+                //     if(ans > s3) ans = s3;
+                //     st.insert(s3);
+                //     q.push(s3);
+                // }
+            }
+        }
+        
+        return ans;
+    }
+};
