2eab4edb37a20beef375e80e924e8841f7cdee84
Badri Vishal Singhal <87443929+vishal8803@users.noreply.github.com>
2023-01-27 13:05:00 +0530

    Time: 513 ms (45.14%), Space: 17.4 MB (56.69%) - LeetHub

---
+++ 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool checkPartitioning(string s) {
+        int n = s.size();
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+        
+        for(int g = 0; g < n; g++) {
+            for(int i = 0, j = g; j < n; j++, i++) {
+                if(g == 0) {
+                    dp[i][j] = true;
+                } else if(g == 1) {
+                    if(s[i] == s[j]) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = false;
+                    }
+                } else {
+                    if(s[i] == s[j] and dp[i+1][j-1]) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = false;
+                    }
+                }
+            }
+        }
+        
+        // for(int i = 0; i < n; i++) {
+        //     for(int j = 0; j < n; j++) {
+        //         cout<<dp[i][j]<<" ";
+        //     }cout<<endl;
+        // }
+        
+        for(int i = 0; i < s.size(); i++) {
+            for(int j = s.size() - 1; j >= 0; j--) {
+                if(dp[0][i] and dp[j][s.size()-1] and dp[i+1][j-1]) return true;
+            }
+        }
+        
+        return false;
+    }
+};
