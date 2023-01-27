5762bb96b355d5dbea931923d59d57164dca922d
Badri Vishal Singhal <87443929+vishal8803@users.noreply.github.com>
2023-01-27 13:06:06 +0530

    Time: 379 ms (62.99%), Space: 17.4 MB (56.69%) - LeetHub

--- 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.cpp
+++ 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.cpp
@@ -24,12 +24,6 @@ public:
             }
         }
         
-        // for(int i = 0; i < n; i++) {
-        //     for(int j = 0; j < n; j++) {
-        //         cout<<dp[i][j]<<" ";
-        //     }cout<<endl;
-        // }
-        
         for(int i = 0; i < s.size(); i++) {
             for(int j = s.size() - 1; j >= 0; j--) {
                 if(dp[0][i] and dp[j][s.size()-1] and dp[i+1][j-1]) return true;
