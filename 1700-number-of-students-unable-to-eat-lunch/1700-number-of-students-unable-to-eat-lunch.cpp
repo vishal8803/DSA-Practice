964880b33d57576a4e9ab5272a8177a19431676f
Badri Vishal Singhal <87443929+vishal8803@users.noreply.github.com>
2024-04-08 09:57:55 +0530

    Time: 5 ms (31.29%), Space: 10.8 MB (79.46%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
@@ -1,33 +1,35 @@
 class Solution {
 public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
-        int zero = 0;
-        int one = 0;
-        list<int> l;
-        for(int i : students) {
-            if(i) one++;
-            else zero++;
-            l.push_back(i);
+        int n = students.size();
+        int no = 0;
+        queue<int> q;
+        for(int student: students) {
+            no += student;
+            q.push(student);
         }
         
-        int j = 0;
-        int cnt = 0;
-        while(j < sandwiches.size() and l.size() > 0) {
-            int num = sandwiches[j];
-            if(num == 0 and zero == 0) break;
-            else if(num == 1 and one == 0) break;
-            int num2 = l.front();
-            if(num == num2) {
-                if(num == 0) zero--;
-                else one--;
-                l.pop_front();
-                j++;
-                cnt++;
+        int nz = n - no;
+        int i = 0;
+        int taken = 0;
+        while(i < sandwiches.size()) {
+            if(sandwiches[i] == 0 and nz == 0) {
+                break;
+            } else if(sandwiches[i] == 1 and no == 0) {
+                break;
+            } else if(sandwiches[i] == q.front()) {
+                if(sandwiches[i] == 0) nz--;
+                else no--;
+                q.pop();
+                i++;
+                taken++;
             } else {
-                l.pop_front();
-                l.push_back(num2);
+                int t = q.front();
+                q.pop();
+                q.push(t);
             }
         }
-        return students.size() - cnt;
+        
+        return n - taken;
     }
 };
